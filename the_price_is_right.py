0e09d4b38cd7ce174979c4b0490e7cab846e487f
Francisco Almeida <fmpapt@gmail.com>
2022-02-27 16:19:05 +0000

    main function create, tk loop working.

--- the_price_is_right.py
+++ the_price_is_right.py
@@ -1,6 +1,25 @@
 """A simple spin the wheel game. There are three players and each one has
 to try to get as close as possible to 100 points without going over."""
 
-import tkinter
+import tkinter as tk
 import random
 
+class Game(tk.Frame):
+    """GUI application for the price is right."""
+
+    def __init__(self, parent):
+        """Initialize GUI."""
+        super().__init__(parent)
+        self.parent = parent
+
+
+def main():
+    """Start here."""
+    root = tk.Tk()
+    root.title("The Price is Right")
+    root.geometry('1280x820')
+    game = Game(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
