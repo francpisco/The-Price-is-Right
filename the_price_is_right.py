a1d9f6064450bf58826ef07fe6198754442a901b
Francisco Almeida <fmpapt@gmail.com>
2022-02-28 18:56:21 +0000

    images smaller and in png format. New event on wheel image.

--- the_price_is_right.py
+++ the_price_is_right.py
@@ -16,8 +16,9 @@ class Game(tk.Frame):
 
     def create_widgets(self):
         """Create widgets."""
-        self.play_button = tk.Button(self.parent, text='Spin!', 
-                                     command=self.spin_wheel)
+        self.play_button = tk.Button(self.parent, text='Roda!', 
+                                     command=self.spin_wheel, 
+                                     height=38, width=10)
         self.play_button.grid(row=0, column=0, sticky='W')
 
         img = tk.PhotoImage(file="imgs/wheel_5.png")
@@ -25,17 +26,22 @@ class Game(tk.Frame):
                                   borderwidth=0)
         self.photo_lbl.grid(row=0, column=1, columnspan=10, sticky='W')
         self.photo_lbl.image = img
+        self.photo_lbl.bind('<B1-Motion>', self.push_the_wheel)
 
     def spin_wheel(self):
-        """Action of spinning the wheel."""
+        """Action of spinning the wheel using a button."""
         print("Spin!")
+
+    def push_the_wheel(self, event):
+        """Spin the wheel using a motion event."""
+        print("Spin!", event.x, event.y)
 
 
 def main():
     """Start here."""
     root = tk.Tk()
     root.title("The Price is Right")
-    root.geometry('1200x880')
+    root.geometry('1200x800')
     game = Game(root)
     root.mainloop()
 
