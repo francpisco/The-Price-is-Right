6ddc3d221372b44b70ce1c0f7f65d4e9a2176d86
Francisco Almeida <fmpapt@gmail.com>
2022-02-28 14:55:51 +0000

    Button and first image added.

--- the_price_is_right.py
+++ the_price_is_right.py
@@ -12,12 +12,30 @@ class Game(tk.Frame):
         super().__init__(parent)
         self.parent = parent
 
+        self.create_widgets()
+
+    def create_widgets(self):
+        """Create widgets."""
+        self.play_button = tk.Button(self.parent, text='Spin!', 
+                                     command=self.spin_wheel)
+        self.play_button.grid(row=0, column=0, sticky='W')
+
+        img = tk.PhotoImage(file="imgs/wheel_5.png")
+        self.photo_lbl = tk.Label(self.parent, image=img, text='',
+                                  borderwidth=0)
+        self.photo_lbl.grid(row=0, column=1, columnspan=10, sticky='W')
+        self.photo_lbl.image = img
+
+    def spin_wheel(self):
+        """Action of spinning the wheel."""
+        print("Spin!")
+
 
 def main():
     """Start here."""
     root = tk.Tk()
     root.title("The Price is Right")
-    root.geometry('1280x820')
+    root.geometry('1200x880')
     game = Game(root)
     root.mainloop()
 
