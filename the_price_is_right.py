92d4057ec091f0ca2a1f409c2e4c7735afd5751d
Francisco Almeida <fmpapt@gmail.com>
2022-03-01 17:58:02 +0000

    Deleted jpgs, added event listener to image, wheel is spinning one number.

--- the_price_is_right.py
+++ the_price_is_right.py
@@ -7,17 +7,22 @@ import random
 class Game(tk.Frame):
     """GUI application for the price is right."""
 
+    wheel_numbers = ('100', '15', '50', '95', '20', '5', '45', '60', '35',
+                     '90', '65', '40', '55', '75', '30', '85', '70', '25',
+                     '80', '10')
+
     def __init__(self, parent):
         """Initialize GUI."""
         super().__init__(parent)
         self.parent = parent
+        self.wheel_position = 0
 
         self.create_widgets()
 
     def create_widgets(self):
         """Create widgets."""
         self.play_button = tk.Button(self.parent, text='Roda!', 
-                                     command=self.spin_wheel, 
+                                     command=self.on_play_button, 
                                      height=38, width=10)
         self.play_button.grid(row=0, column=0, sticky='W')
 
@@ -26,15 +31,36 @@ class Game(tk.Frame):
                                   borderwidth=0)
         self.photo_lbl.grid(row=0, column=1, columnspan=10, sticky='W')
         self.photo_lbl.image = img
-        self.photo_lbl.bind('<B1-Motion>', self.push_the_wheel)
+        self.photo_lbl.bind('<Button-1>', self.mouse_bt_pressed)
+        self.photo_lbl.bind('<ButtonRelease-1>', self.mouse_bt_released)
 
-    def spin_wheel(self):
+    def on_play_button(self):
         """Action of spinning the wheel using a button."""
         print("Spin!")
 
-    def push_the_wheel(self, event):
-        """Spin the wheel using a motion event."""
-        print("Spin!", event.x, event.y)
+    def mouse_bt_pressed(self, event):
+        """Callback function for clicking button on the wheel."""
+        print("SSpinnig!", event.x, event.y)
+    
+    def mouse_bt_released(self, event):
+        """Callback function for release button on wheel."""
+        print("Spin that wheel!!!", event.x, event.y)
+        self.spin_wheel()
+
+    def update_image(self):
+        """Update wheel image."""
+        file_name = f"imgs/wheel_{self.wheel_numbers[self.wheel_position]}.png"
+        img = tk.PhotoImage(file=file_name)
+        self.photo_lbl.config(image=img)
+        self.photo_lbl.image = img
+        if self.wheel_position < (len(self.wheel_numbers) - 1):
+            self.wheel_position += 1
+        else:
+            self.wheel_position = 0
+
+    def spin_wheel(self):
+        """Make wheel spin."""
+        self.parent.after(100, self.update_image)
 
 
 def main():
