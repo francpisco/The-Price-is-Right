fb0c64966739b0e454328c19a2941727eb940d79
Francisco Almeida <fmpapt@gmail.com>
2022-03-02 12:03:40 +0000

    Pre-load wheel images.

--- the_price_is_right.py
+++ the_price_is_right.py
@@ -3,12 +3,11 @@ to try to get as close as possible to 100 points without going over."""
 
 import tkinter as tk
 import time
-import random
+import math
 
-WHEEL_SPIN_MULTIPLIER = 20_000
-WHEEL_STOP_STEP = 600
-WHEEL_MIN_STEP = 5
-WHEEL_DAMPENING = 1.1
+WHEEL_SPIN_MULTIPLIER = 50_000
+WHEEL_STOP_STEP = 500
+WHEEL_DAMPENING = 1.03
 
 class Game(tk.Frame):
     """GUI application for the price is right."""
@@ -24,6 +23,7 @@ class Game(tk.Frame):
         self.wheel_position = 0
 
         self.create_widgets()
+        self.create_wheel_imgs()
 
     def create_widgets(self):
         """Create widgets."""
@@ -32,13 +32,22 @@ class Game(tk.Frame):
                                      height=38, width=10)
         self.play_button.grid(row=0, column=0, sticky='W')
 
-        img = tk.PhotoImage(file="imgs/wheel_5.png")
+        img = tk.PhotoImage(file="imgs/wheel_100.png")
         self.photo_lbl = tk.Label(self.parent, image=img, text='',
                                   borderwidth=0)
         self.photo_lbl.grid(row=0, column=1, columnspan=10, sticky='W')
         self.photo_lbl.image = img
         self.photo_lbl.bind('<Button-1>', self.mouse_bt_pressed)
         self.photo_lbl.bind('<ButtonRelease-1>', self.mouse_bt_released)
+
+    def create_wheel_imgs(self):
+        """Load wheel images into tuple."""
+        wheel_imgs_list = []
+        for wheel_number in self.wheel_numbers:
+            file_name = f"imgs/wheel_{wheel_number}.png"
+            img = tk.PhotoImage(file=file_name)
+            wheel_imgs_list.append(img)
+        self.wheel_imgs = tuple(wheel_imgs_list)
 
     def on_play_button(self):
         """Action of spinning the wheel using a button."""
@@ -60,10 +69,8 @@ class Game(tk.Frame):
 
     def spin_wheel(self):
         """Make wheel spin using sequence of images."""
-        file_name = f"imgs/wheel_{self.wheel_numbers[self.wheel_position]}.png"
-        img = tk.PhotoImage(file=file_name)
-        self.photo_lbl.config(image=img)
-        self.photo_lbl.image = img
+        self.photo_lbl.config(image=self.wheel_imgs[self.wheel_position])
+        self.photo_lbl.image = self.wheel_imgs[self.wheel_position]
         if self.wheel_position < (len(self.wheel_numbers) - 1):
             self.wheel_position += 1
         else:
@@ -75,13 +82,12 @@ class Game(tk.Frame):
         difference between mouse button pressed and released and y positions
         of mouse."""
         self.wheel_spin_factor = self.wheel_dif_time / self.wheel_dif_y
-        self.step = round(self.wheel_spin_factor * WHEEL_SPIN_MULTIPLIER)
-        if self.step < WHEEL_MIN_STEP:
-            self.step = WHEEL_MIN_STEP
+        self.step = math.ceil(self.wheel_spin_factor * WHEEL_SPIN_MULTIPLIER)
 
     def push_wheel(self):
         """Control movement of wheel."""
-        self.step = round(self.step * WHEEL_DAMPENING)
+        self.step = math.ceil(self.step * WHEEL_DAMPENING)
+        print(self.step)
         if self.step < WHEEL_STOP_STEP:
             self.parent.after(self.step, self.spin_wheel)
 
